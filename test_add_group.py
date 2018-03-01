bffb833892475b401f7d8b845d6ed04d67202348
kerimov <kerimov225@mail.ru>
2018-03-01 21:31:43 +0300

    Changed line to run test on Firefox ESR

--- test_add_group.py
+++ test_add_group.py
@@ -12,13 +12,13 @@ def is_alert_present(wd):
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
