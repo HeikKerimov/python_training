135fa73c8d25189b3a7c735c713f8f6900f46e0e
kerimov <kerimov225@mail.ru>
2018-03-08 14:10:43 +0300

    Added contact helper

--- fixture/contact_application.py
+++ fixture/contact_application.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.contact import ContactHelper
 from fixture.session import SessionHelper
 
 
@@ -8,24 +9,7 @@ class ContactApplication:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_add_new_contact_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.phone)
-        wd.find_element_by_name("submit").click()
-
-    def open_add_new_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,6 +13,6 @@ def app(request):
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="Heik", last_name="Kerimov", phone="9214410883"))
+    app.contact.create(Contact(first_name="Heik", last_name="Kerimov", phone="9214410883"))
     app.session.logout()
 
