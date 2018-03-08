68440b8eb55640b0d798c59e1f7dd753fb30da7d
kerimov <kerimov225@mail.ru>
2018-03-08 14:08:26 +0300

    Added group helper

---
+++ fixture/group.py
@@ -0,0 +1,28 @@
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
