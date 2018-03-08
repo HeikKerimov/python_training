b0e592026e8beac2fac392f28f32a3996d4551d2
kerimov <kerimov225@mail.ru>
2018-03-08 15:28:42 +0300

    Added test to delete contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -17,6 +17,27 @@ class ContactHelper:
         wd.find_element_by_name("home").send_keys(contact.phone)
         wd.find_element_by_name("submit").click()
 
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.phone)
+        wd.find_element_by_name("update").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
     def open_add_new_contact_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
---
+++ test/test_delete_contact.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def test_delete_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.delete_first_contact()
+    app.session.logout()
+
