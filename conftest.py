c37d633557217f5965559eea90677ef17a797dd3
kerimov <kerimov225@mail.ru>
2018-03-30 18:27:39 +0300

    Added options for command line

--- conftest.py
+++ conftest.py
@@ -6,15 +6,17 @@ fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
 
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
 
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
 
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
@@ -29,3 +31,8 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -1,16 +1,27 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(ChromeDriverManager().install())
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +33,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("searchstring")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def close(self):
         self.wd.quit()
