232aa6bff15352a99263e4337917acd475e29a58
kerimov <kerimov225@mail.ru>
2018-04-05 19:30:19 +0300

    Implemented group generator

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from data.add_group import constant as test_data
-import pytest
 
 
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
